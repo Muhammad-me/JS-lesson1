9e304079920a2ef04f9b350ec9deb676c448f076
Muhammad-me <mmajidhanov@gmail.com>
2022-06-10 06:33:26 +0500

    New lesson on JS

---
+++ index.js
@@ -0,0 +1,37 @@
+// string + bu qo'shtirnoq  ichida yaratilgan har qanday belgi
+// number + 21345246453
+// boling + true yoki false
+// undefined + undefined
+// MaN + not a number
+
+// var box = 12345568;
+// console.log(box);
+// box = "Hello what's up";
+// console.log(box);
+
+// var a = "6";
+// var b = 5;
+// console.log(a * b);
+
+// var ism = prompt("Ismingizni kiriting");
+// // console.log(ism);
+// var ism = prompt("Ismingizni kiriting");
+// alert("Salom " + ism + " saytga hush kelibsiz!");
+
+
+
+var ism = prompt("Hello Welcome To My Site. Please Write Your Name!");
+console.log("Please Write Your Age");
+var yil = prompt("Please Write Your Age!");
+if (yil < 2004){
+    alert("Welcome To My Site!");
+}
+if (yil > 2004){
+    alert("You are not old enough.");
+}
+
+
+
+
+
+
